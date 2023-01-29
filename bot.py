29dd53d77790a8fc0897de10967d04f7d46170d8
Lepskie Zakhar <lepskiezakhar@gmail.com>
2023-01-29 21:58:44 +0300

    register handler for other updates

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher, executor
 from configs import load_config
-from handlers import register_user_handlers
+from handlers import register_user_handlers, register_other_handlers
 
 
 config = load_config('.env')
@@ -10,6 +10,8 @@ dp: Dispatcher = Dispatcher(bot)
 
 
 register_user_handlers(dp=dp)
+register_other_handlers(dp=dp)
+
 
 if __name__ == '__main__':
     executor.start_webhook(dp, skip_updates=True)
